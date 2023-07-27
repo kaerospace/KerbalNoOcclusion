27018e3244127ce8fe55464788da9d31e1ea7764
kAerospace <54720118+kaerospace@users.noreply.github.com>
2023-07-27 14:40:55 +0200

    Initial Commit

---
+++ main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace FuckIntakeOcclusion
+{
+    [KSPModule("Unoccludable Intake")]
+    public class ModuleUnoccludableIntake : ModuleResourceIntake
+    {
+        public new void FixedUpdate()
+        {
+            part.ShieldedFromAirstream = false;
+            base.FixedUpdate();
+        }
+    }
+}
