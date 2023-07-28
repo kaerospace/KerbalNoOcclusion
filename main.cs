452277b7f83b3248c18837100aa22e3bd79b5b97
kAerospace <54720118+kaerospace@users.noreply.github.com>
2023-07-28 11:28:15 +0200

    Add files via upload

--- main.cs
+++ main.cs
@@ -4,14 +4,18 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
-namespace FuckIntakeOcclusion
+namespace NoIntakeOcclusion
 {
     [KSPModule("Unoccludable Intake")]
     public class ModuleUnoccludableIntake : ModuleResourceIntake
     {
+        [KSPField(guiName = "Active while Shielded", isPersistant = true, guiActiveEditor = true, guiActive = false)]
+        [UI_Toggle(enabledText = "Enabled", disabledText = "Disabled")]
+        public bool enableShielded = false;
+
         public new void FixedUpdate()
         {
-            part.ShieldedFromAirstream = false;
+            if (enableShielded) part.ShieldedFromAirstream = false;
             base.FixedUpdate();
         }
     }
